dc49c4733573f52073f981c05f285c481aaa8cd8
shashwat0105 <imt_2019093@iiitm.ac.in>
2021-05-20 21:17:09 +0530

    Added kadane's algo

--- Week 1/Arrays/Kadane'sAlgo-LargestSumSubArray.cpp
+++ Week 1/Arrays/Kadane'sAlgo-LargestSumSubArray.cpp
@@ -1,2 +1,16 @@
 // (Modified) Kadane's Algorithm
 // Watch https://www.youtube.com/watch?v=YxuK6A3SvTs
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {-1, 2, 4, -3, 5, 2, -5, 2};
+    int best = 0, sum =0;
+    for(int k=0; k< 8; k++){
+        sum = max(arr[k], sum+arr[k]);
+        best = max(best, sum);
+    }
+    cout<<best;
+    return 0;
+}
