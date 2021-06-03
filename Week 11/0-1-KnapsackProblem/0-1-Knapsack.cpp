4eb7e679cd603e0df16fc700827f93680c088b32
shashwat0105 <imt_2019093@iiitm.ac.in>
2021-06-03 19:42:56 +0530

    Added method 3 of knapsack(memoisation/topdown)

--- Week 11/0-1-KnapsackProblem/0-1-Knapsack.cpp
+++ Week 11/0-1-KnapsackProblem/0-1-Knapsack.cpp
@@ -105,10 +105,10 @@ for(int i = 0; i < n+1; i++){
 for ( i = 1; i < n+1; i++){
     for ( j = 1; j< W+1; j++){
         if(wt[i-1]<=j){
-            t[i][j] = max(val[i-1]+ t[i-1][j-wt[i-1]], t[i-1][j]);  
+            return t[i][j] = max(val[i-1]+ t[i-1][j-wt[i-1]], t[i-1][j]);  
         }
         else{
-            t[i][j] = t[i-1][j];
+            return t[i][j] = t[i-1][j];
         }
     }
 }
