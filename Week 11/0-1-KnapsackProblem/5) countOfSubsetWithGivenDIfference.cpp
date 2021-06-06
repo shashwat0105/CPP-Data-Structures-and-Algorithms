f795b803bc13291a5d97487373e57952054b9e2a
shashwat0105 <imt_2019093@iiitm.ac.in>
2021-06-07 01:22:19 +0530

    added target sum problem

---
+++ Week 11/0-1-KnapsackProblem/5) countOfSubsetWithGivenDIfference.cpp
@@ -0,0 +1,13 @@
+// Problem statement arr[] : {1, 1, 2, 3}, difference = 1  // these are different 1's
+// O/P :- 3 -> {{1,2},{1,3}}, {{1,2},{1,3}}, {{1,1,2},{3}} = No. of pair of subsets whose difference of sum is 1 ie S2 - S1 = 1 
+// S2 - S1 = diff -> (i)
+// S2 + S1 = Sum of array -> (ii)
+// Adding (i) & (ii)
+// S2 = (diff + Sum of array)/2
+// diff is given in ques, sum of array could be found
+// Hence our problem is reduced to find no of subsets whose sum is (1+7)/2 = 4, which is nothing but count of subset sum problem(Wow)
+
+int sum = diff + (Sum of array - diff)/2;
+
+return countOfSubsetSum(arr, sum);
+
---
+++ Week 11/0-1-KnapsackProblem/6) targetSum(leetcode).cpp
@@ -0,0 +1,7 @@
+// Problem statement arr[] : {1, 1, 2, 3}, Sum = 1, We need to put + or - sign before element of array and get the sum = 1
+// For ex:- +1-1-2+3 = 1 , -1+1-2+3 = 1, +1+1+2-3 = 1, & then count the number of such combos, Here O/P = 3
+// Now:- +1-1-2+3 = (+1+3)-(+1+2) -> Same as difference of sum of 2 subsets.
+// Hence our problem reduced to Count Of Subset With Given Difference problem (WOW). Diff of that prob is named as sum here, thats it!
+// Just the way of asking is different ans is exact same!
+
+
