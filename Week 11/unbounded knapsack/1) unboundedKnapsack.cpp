6edf75f4512d698aefc9796f9d065bf6d18a886e
shashwat0105 <imt_2019093@iiitm.ac.in>
2021-06-12 22:29:49 +0530

    Addedunbounded example

--- Week 11/unbounded knapsack/1) unboundedKnapsack.cpp
+++ Week 11/unbounded knapsack/1) unboundedKnapsack.cpp
@@ -1,7 +1,9 @@
 // https://www.geeksforgeeks.org/unbounded-knapsack-repetition-items-allowed/
 // Multiple occurences of same item is allowed
 
-
+// For example:- The item is icecream:- 
+// 1) I dont like icecream -> So, I wont take it regardless of the number of times you offer me.
+// 2) I like icecream -> I can take it any number of times you offer me.
 
 
 
