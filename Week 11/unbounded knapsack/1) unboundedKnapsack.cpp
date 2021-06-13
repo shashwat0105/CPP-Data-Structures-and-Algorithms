05bfd0b764b8661b35bfb7c1ee503e360ebf52d0
shashwat0105 <imt_2019093@iiitm.ac.in>
2021-06-13 11:34:01 +0530

    added unbounded knapsack code

--- Week 11/unbounded knapsack/1) unboundedKnapsack.cpp
+++ Week 11/unbounded knapsack/1) unboundedKnapsack.cpp
@@ -2,10 +2,52 @@
 // Multiple occurences of same item is allowed
 
 // For example:- The item is icecream:- 
-// 1) I dont like icecream -> So, I wont take it regardless of the number of times you offer me.
-// 2) I like icecream -> I can take it any number of times you offer me.
+// 1) I dont like icecream -> So, I wont take it regardless of the number of times you offer me. (Processed)
+// 2) I like icecream -> I can take it any number of times you offer me. (Unprocessed)
+
+// 0/1 -> Item -> 1) Taken -> Processed -> n-1 -> i-1
+//                2) Not Taken -> Processed -> n-1 -> i-1
+// unbounded -> Item -> 1) Taken -> Unprocessed -> n -> i  (Here is the code variation only)
+//                      2) Not Taken -> Processed -> n-1 -> i-1
 
 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int unboundedKnapSack(int wt[], int val[], int W, int n)
+{
 
-
+    int t[n + 1][W + 1];
+ 
+    // Build table K[][] in bottom up manner
+    for(int i = 0; i <= n; i++)
+    {
+        for(int j = 0; j <= W; j++)
+        {
+            // base condition
+            if (i == 0 || j == 0)
+                t[i][j] = 0;
+            // choice diagram
+            else if (wt[i - 1] <= j)
+                t[i][j] = max(val[i - 1] +
+                                t[i][j - wt[i - 1]],  // the only change i-1 -> i
+                                t[i - 1][j]);
+            else
+                t[i][j] = t[i - 1][j];
+        }
+    }
+    return t[n][W];
+}
+ 
+int main()
+{
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+     
+    cout << unboundedKnapSack(wt, val, W, n);
+     
+    return 0;
+}
 
