fa88a8ee56c01a7c7726e9942cc397b49029ec91
shashwat0105 <imt_2019093@iiitm.ac.in>
2022-02-09 12:22:26 +0530

    Added some problems

--- Week 3 (String & Its algorithms)/String & String Algorithms/Practice Problems/05) romanToInteger.cpp
+++ Week 3 (String & Its algorithms)/String & String Algorithms/Practice Problems/05) romanToInteger.cpp
@@ -28,3 +28,28 @@ public:
         return res;
     }
 };
+
+// Alternate solution using map
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res=0;
+        int n = s.size();
+        
+        map<char, int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        for(int i=0; i<n; ++i){
+            if(i+1<n && m[s[i]]<m[s[i+1]]) res-= m[s[i]];
+            else res+= m[s[i]];
+        }
+        return res;
+    }
+};
+
